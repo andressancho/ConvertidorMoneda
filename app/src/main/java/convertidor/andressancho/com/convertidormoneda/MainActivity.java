4e03a5c978ea9e0eacbae11a3059edb8935abbd1
Andres Sancho <andrem.san12@gmail.com>
2018-02-23 21:40:14 -0600

    Libreria

--- app/src/main/java/convertidor/andressancho/com/convertidormoneda/MainActivity.java
+++ app/src/main/java/convertidor/andressancho/com/convertidormoneda/MainActivity.java
@@ -2,6 +2,7 @@ package convertidor.andressancho.com.convertidormoneda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.ksoap2.serialization.;
 
 public class MainActivity extends AppCompatActivity {
 
